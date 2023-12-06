0ceefb67a8382143a05d28144031d1ebed6b34a9
jahearn3 <josephahearn3@gmail.com>
2023-12-06 14:32:53 -0800

    reworked input processing for a cleaner part 2

--- day06.py
+++ day06.py
@@ -5,15 +5,12 @@ import os
 
 f = os.path.basename(__file__)
 day = f[3:5]
-
 data = ld.load_data(f'example{day}.txt')
 data = ld.load_data(f'input{day}.txt')
 
 ans = 1
-
 times = list(map(int, data[0].split(':')[1].split()))
 dists = list(map(int, data[1].split(':')[1].split()))
-
 
 for i in range(len(times)):
     count = 0
@@ -26,14 +23,12 @@ for i in range(len(times)):
 print(ans)
 
 # Part 2
-
-t = 50748685
-dist = 242101716911252
-count = 0
+t = int(data[0].split(':')[1].strip().replace(' ',''))
+dist = int(data[1].split(':')[1].strip().replace(' ',''))
+ans = 0
 for j in range(t):
     v = j
     d = v * (t - j)
     if d > dist:
-        count += 1
-print(count)
-# not 48
+        ans += 1
+print(ans)
