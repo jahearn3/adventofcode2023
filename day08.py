73183e9f3ce54fe5b2ec14f387cae0d6edcb5255
jahearn3 <josephahearn3@gmail.com>
2023-12-08 07:53:11 -0800

    day 8 tidying up the code

--- day08.py
+++ day08.py
@@ -2,21 +2,26 @@
 
 import load_data as ld 
 import os 
+import math
+
+def get_network(data):
+    network = {}
+    for line in data[2:]:
+        a, b = line.split(' = ')
+        l, r = b.strip('()').split(',')
+        r = r.strip()
+        network[a] = (l, r)
+    return network
 
 f = os.path.basename(__file__)
 day = f[3:5]
 
 data = ld.load_data(f'example{day}.txt')
-# data = ld.load_data(f'example{day}b.txt')
-# data = ld.load_data(f'input{day}.txt')
+data = ld.load_data(f'example{day}b.txt')
+data = ld.load_data(f'input{day}.txt')
 
 instructions = data[0]
-network = {}
-for line in data[2:]:
-    a, b = line.split(' = ')
-    l, r = b.strip('()').split(',')
-    r = r.strip()
-    network[a] = (l, r)
+network = get_network(data)
 
 start = 'AAA'
 cur = 'AAA'
@@ -24,9 +29,7 @@ end = 'ZZZ'
 ans = 0
 
 while cur != end:
-    for i, char in enumerate(instructions):
-        if cur == end:
-            break
+    for char in instructions:
         if char == 'L':
             next = network[cur][0]
         elif char == 'R':
@@ -41,13 +44,9 @@ data = ld.load_data(f'example{day}c.txt')
 data = ld.load_data(f'input{day}.txt')
 
 instructions = data[0]
-network = {}
-for line in data[2:]:
-    a, b = line.split(' = ')
-    l, r = b.strip('()').split(',')
-    r = r.strip()
-    network[a] = (l, r)
+network = get_network(data)
 
+# Get starting points and ending points
 start = []
 end = []
 for k in network.keys():
@@ -56,43 +55,9 @@ for k in network.keys():
     elif k[-1] == 'Z':
         end.append(k)
 
-print(start, end)
-
-# cur = start
-# ans = 0
-# zs = 0
-# maxzs = 0
-
-# while zs != len(start):
-#     for char in instructions:
-#         next = []
-#         if char == 'L':
-#             for c in cur:
-#                 next.append(network[c][0])
-#         elif char == 'R':
-#             for c in cur:
-#                 next.append(network[c][1])
-#         # print(next)
-#         cur = next
-#         zs = 0
-#         for c in cur:
-#             if c[-1] == 'Z':
-#                 zs += 1
-#         ans += 1
-#         if zs > maxzs:
-#             maxzs = zs
-#             print(zs, ans, cur)
-
-# print(ans)
-
-# 293 incorrect
-
-# Could find least common multiple 
-# but don't know which start maps to which end, 
-# so would need to do it for all
-
+# Get the first solution for each starting point
+ss = []
 for s in start:
-    # for e in end:
     cur = s
     ans = 0
     while cur[-1] != 'Z':
@@ -101,28 +66,15 @@ for s in start:
                 next = network[cur][0]
             elif char == 'R':
                 next = network[cur][1]
-            # print(next)
             cur = next
             ans += 1
             if cur[-1] == 'Z':
-                print(s, cur, ans)
+                ss.append(ans)
                 break
 
-import math
-a = 17873
-b = 19631
-c = 17287
-d = 12599
-e = 21389
-f = 20803
-lcm1 = (a*b)/math.gcd(a,b)
-print(lcm1)
-lcm2 = (c*d)/math.gcd(c,d)
-print(lcm2)
-lcm3 = (e*f)/math.gcd(e,f)
-print(lcm3)
-lcm4 = (int(lcm1)*int(lcm2))/math.gcd(int(lcm1),int(lcm2))
-print(lcm4)
-lcm5 = (int(lcm3)*int(lcm4))/math.gcd(int(lcm3),int(lcm4))
-print(lcm5)
-            
+# Find the least common multiple of the combination
+while len(ss) > 1:
+    a = ss.pop(0)
+    b = ss.pop(0)
+    ss.append(int((a * b) / math.gcd(a, b)))
+print(ss[0])
