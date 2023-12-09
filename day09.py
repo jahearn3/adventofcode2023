f93909c9efb581d5ed133cadcb45c44ecaa44428
jahearn3 <josephahearn3@gmail.com>
2023-12-09 08:33:17 -0800

    day 9 part 2 and tidied up code

--- day09.py
+++ day09.py
@@ -10,7 +10,7 @@ data = ld.load_data(f'example{day}.txt')
 data = ld.load_data(f'input{day}.txt')
 
 ans = 0
-
+part2 = True # Set to False to get part 1 answer
 for line in data:
     vals = list(map(int, line.split(' ')))
     zeros = 0
@@ -29,15 +29,15 @@ for line in data:
     sequences[f'{len(vals)}'] = vals
     # Start extrapolating
     sequences[f'{len(vals)}'].append(0)
-    # print(sequences)
-    # print(len(vals), n)
     for i in range(len(vals), n + 1):
         lst = sequences[f'{i}']
         sub = sequences[f'{i-1}']
-        # print(lst)
-        # print(sub)
-        nxt = lst[-1] + sub[-1]
-        sequences[f'{i}'].append(nxt)
+        if part2:
+            nxt = lst[0] - sub[0]
+            sequences[f'{i}'].insert(0, nxt)
+        else:
+            nxt = lst[-1] + sub[-1]
+            sequences[f'{i}'].append(nxt)
         if i == n:
             print(nxt)
             ans += nxt
