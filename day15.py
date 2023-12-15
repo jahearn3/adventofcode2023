f045e76dfc6aaaa1ad4d1cc820ec27d6ef301ccb
jahearn3 <josephahearn3@gmail.com>
2023-12-14 23:03:39 -0800

    day 15 part 1

--- day15.py
+++ day15.py
@@ -1,4 +1,4 @@
-# Day 15: 
+# Day 15: Lens Library
 
 import load_data as ld 
 import os 
@@ -9,8 +9,15 @@ day = f[3:5]
 data = ld.load_data(f'example{day}.txt')
 data = ld.load_data(f'input{day}.txt')
 
+lst = data[0].split(',')
+# lst = ['H', 'A', 'S', 'H']
 ans = 0
-
-
+for item in lst:
+    a = 0
+    for char in item:
+        a += ord(char)
+        a *= 17
+        a %= 256
+    ans += a
 
 print(ans)
