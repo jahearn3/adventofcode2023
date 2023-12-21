fc8fea1f63b554a2adc50225e2ec8cc2b81af274
jahearn3 <josephahearn3@gmail.com>
2023-12-21 14:06:17 -0800

    day 21 part 1 complete

---
+++ day21.py
@@ -0,0 +1,38 @@
+# Day 21: Step Counter
+
+import load_data as ld 
+import os 
+
+f = os.path.basename(__file__)
+day = f[3:5]
+
+data = ld.load_data(f'example{day}.txt')
+data = ld.load_data(f'input{day}.txt')
+
+for i, line in enumerate(data):
+    for j, char in enumerate(line):
+        if char == 'S':
+            start = (j, i)
+            xmax = len(line)
+            ymax = len(data)
+
+garden = ['.', 'S']
+q = [start]
+final_step = 64
+for i in range(final_step):
+    next = []
+    while len(q) > 0:
+        x, y = q.pop(0)
+        if x > 0 and data[y][x-1] in garden:
+            next.append((x-1, y))
+        if x + 1 < xmax and data[y][x+1] in garden:
+            next.append((x+1, y))
+        if y > 0 and data[y-1][x] in garden:
+            next.append((x, y-1))
+        if y + 1 < ymax and data[y+1][x] in garden:
+            next.append((x, y+1))
+    q = list(set(next))
+    if i + 1 == final_step:
+        print(len(q))
+        break 
+
