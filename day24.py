2650ff984c6e713725332cae2808c9b1eadcf4c6
jahearn3 <josephahearn3@gmail.com>
2024-01-04 13:18:37 -0800

    day 24 part 1

--- day24.py
+++ day24.py
@@ -7,7 +7,7 @@ f = os.path.basename(__file__)
 day = f[3:5]
 
 data_source = f'example{day}.txt'
-# data_source = f'input{day}.txt'
+data_source = f'input{day}.txt'
 data = ld.load_data(data_source)
 
 hailstones = []
@@ -16,7 +16,7 @@ for line in data:
     pos, vel = line.split('@')
     x, y, z = list(map(int, pos.strip().split(',')))
     u, v, w = list(map(int, vel.strip().split(',')))
-    hailstones.append((x, y, x + u, y + v)) # ignoring z-axis for part 1
+    hailstones.append((x, y, u, v)) 
 
 if data_source[0] == 'e':
     xmin, xmax = 7, 27
@@ -28,29 +28,24 @@ elif data_source[0] == 'i':
 
 ans = 0
 
+# Using insight from HyperNeutrino
 for i, h1 in enumerate(hailstones):
     for j, h2 in enumerate(hailstones):
         if i < j:
-            # Evaluate whether h1 and h2 cross paths
-            # using line-line intersection
-            x1, y1, x2, y2 = h1
-            print(x1, y1, x2, y2)
-            x3, y3, x4, y4 = h2
-            print(x3, y3, x4, y4)
-            # Calculate denominator first to check if it is zero
-            d = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4))
-            if d == 0:
-                print('paths are parallel or coincident')
-            else:
-                xc = (((x1 * y2) - (y1 * x2)) * (x3 - x4)) - ((x1 * x2) * ((x3 * y4) - (y3 * x4)))
-                xc /= d 
-                yc = (((x1 * y2) - (y1 * x2)) * (y3 - y4)) - ((y1 * y2) * ((x3 * y4) - (y3 * x4)))
-                yc /= d
-                print(xc, yc)
-                if xmin <= xc <= xmax and ymin <= yc <= ymax:
+            x1, y1, u1, v1 = h1
+            a1 = v1
+            b1 = -u1
+            c1 = v1 * x1 - u1 * y1
+            x2, y2, u2, v2 = h2
+            a2 = v2
+            b2 = -u2
+            c2 = v2 * x2 - u2 * y2
+            if a1 * b2 == a2 * b1:
+                continue
+            x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1)
+            y = (c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1)
+            if xmin <= x <= xmax and ymin <= y <= ymax:
+                if (x - x1) * u1 >= 0 and (y - y1) * v1 >= 0 and (x - x2) * u2 >= 0 and (y - y2) * v2 >= 0:
                     ans += 1
-            
-
-
-
+     
 print(ans)
